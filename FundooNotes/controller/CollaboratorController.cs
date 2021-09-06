99295d1b2ded1f9181012e4d6357038ac28cd617
Jebakani <vjishwarya@gmail.com>
2021-09-06 12:30:51 +0530

    [JEBAKANI] Refactor: Refactoring the collaborator controlller by adding authorization

--- FundooNotes/controller/CollaboratorController.cs
+++ FundooNotes/controller/CollaboratorController.cs
@@ -1,4 +1,5 @@
 ﻿using Manager.Interface;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using System;
@@ -8,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace FundooNotes.Controller
 {
+    [Authorize]
     public class CollaboratorController : ControllerBase
     {
         private readonly ICollaboratorManager manager;
